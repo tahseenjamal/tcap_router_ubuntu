bd3a0c834c71bca4bba01b971741c51fcefe51d1
Tahseen Jamal <tahseen.jamal@gmail.com>
2026-03-22 00:26:50 +0530

    Updated

--- sigtran/sigtran_stack.c
+++ sigtran/sigtran_stack.c
@@ -3,7 +3,6 @@
 #include <osmocom/core/msgb.h>
 #include <osmocom/core/prim.h>
 #include <osmocom/sigtran/osmo_ss7.h>
-#include <osmocom/sigtran/osmo_ss7_as.h>
 #include <osmocom/sigtran/osmo_ss7_asp.h>
 #include <osmocom/sigtran/sccp_sap.h>
 
@@ -46,52 +45,50 @@ static int sccp_prim_cb(struct osmo_prim_hdr *oph, void *ctx) {
 
 /* ========================================= */
 
-void sigtran_start()
-{
-    printf("Initializing SIGTRAN → STP\n");
+void sigtran_start() {
+  printf("Initializing SIGTRAN → STP\n");
 
-    osmo_ss7_init();
+  osmo_ss7_init();
 
-    ss7 = osmo_ss7_instance_find_or_create(NULL, 0);
-    if (!ss7) {
-        printf("SS7 instance failed\n");
-        return;
-    }
+  ss7 = osmo_ss7_instance_find_or_create(NULL, 0);
+  if (!ss7) {
+    printf("SS7 instance failed\n");
+    return;
+  }
 
-    osmo_ss7_instance_set_pc(ss7, 0x010101);
+  osmo_ss7_instance_set_pc(ss7, 0x010101);
 
-    /* ASP (M3UA client) */
-    asp = osmo_ss7_asp_create(ss7, "tcap-asp", OSMO_SS7_ASP_PROT_M3UA);
-    if (!asp) {
-        printf("ASP create failed\n");
-        return;
-    }
+  /* ASP (M3UA client) */
+  asp = osmo_ss7_asp_create(ss7, "tcap-asp", OSMO_SS7_ASP_PROT_M3UA);
+  if (!asp) {
+    printf("ASP create failed\n");
+    return;
+  }
 
-    osmo_ss7_asp_set_peer(asp, "127.0.0.1", 2905);
-    osmo_ss7_asp_set_rctx(asp, 0);
-    osmo_ss7_asp_set_traffic_mode(asp, OSMO_SS7_TM_OVERRIDE);
+  osmo_ss7_asp_set_peer(asp, "127.0.0.1", 2905);
+  osmo_ss7_asp_set_rctx(asp, 0);
+  osmo_ss7_asp_set_traffic_mode(asp, OSMO_SS7_TM_OVERRIDE);
 
-    if (osmo_ss7_asp_start(asp) != 0) {
-        printf("ASP start failed\n");
-        return;
-    }
+  if (osmo_ss7_asp_start(asp) != 0) {
+    printf("ASP start failed\n");
+    return;
+  }
 
-    printf("M3UA ASP connected\n");
+  printf("M3UA ASP connected\n");
 
-    /* SCCP */
-    sccp = osmo_sccp_instance_create(ss7, NULL);
-    if (!sccp) {
-        printf("SCCP create failed\n");
-        return;
-    }
+  /* SCCP */
+  sccp = osmo_sccp_instance_create(ss7, NULL);
+  if (!sccp) {
+    printf("SCCP create failed\n");
+    return;
+  }
 
-    sccp_user = osmo_sccp_user_bind(sccp, "tcap-router",
-                                   sccp_prim_cb, 146);
+  sccp_user = osmo_sccp_user_bind(sccp, "tcap-router", sccp_prim_cb, 146);
 
-    if (!sccp_user) {
-        printf("SCCP bind failed\n");
-        return;
-    }
+  if (!sccp_user) {
+    printf("SCCP bind failed\n");
+    return;
+  }
 
-    printf("TCAP router ready (SSN 146)\n");
+  printf("TCAP router ready (SSN 146)\n");
 }
